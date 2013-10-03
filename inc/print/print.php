77654650f03f6fcda1c3f3035c58c410dfcf9b78
Miguel Peixe <miguel@cardume.art.br>
2013-10-03 13:01:10 -0300

    start print functionality

---
+++ inc/print/print.php
@@ -0,0 +1,91 @@
+<?php
+
+/*
+ * Oxpeckers
+ * Print
+ */
+
+class Oxpeckers_Print {
+
+	function __construct() {
+
+		add_action('init', array($this, 'init'));
+
+	}
+
+	function init() {
+
+		add_filter('query_vars', array($this, 'query_vars'));
+		add_action('template_redirect', array($this, 'template_redirect'));
+
+	}
+
+	function query_vars($vars) {
+		$vars[] = 'print';
+		return $vars;
+	}
+
+	function template_redirect() {
+		global $wp_query;
+		if($wp_query->get('print') && is_single()) {
+			wp_enqueue_style('oxpeckers-print', get_stylesheet_directory_uri() . '/inc/print/print.css');
+			if(have_posts()) :
+				while(have_posts()) :
+					the_post();
+					$this->template_header();
+					?>
+					<div id="oxpeckers-print">
+						<h2><?php bloginfo('name'); ?></h2>
+						<h1><?php the_title(); ?></h1>
+						<?php the_content(); ?>
+					</div>
+					<?php
+					$this->template_footer();
+				endwhile;
+			endif;
+			exit;
+		}
+	}
+
+	function template_header() {
+		?>
+		<!DOCTYPE html>
+		<html <?php language_attributes(); ?>>
+		<head>
+		<meta charset="<?php bloginfo('charset'); ?>" />
+		<title><?php
+			global $page, $paged;
+
+			wp_title( '|', true, 'right' );
+
+			bloginfo( 'name' );
+
+			$site_description = get_bloginfo('description', 'display');
+			if ( $site_description && ( is_home() || is_front_page() ) )
+				echo " | $site_description";
+
+			/*if ( $paged >= 2 || $page >= 2 )
+				echo ' | Página ' . max($paged, $page);*/
+
+			?></title>
+		<link rel="profile" href="http://gmpg.org/xfn/11" />
+		<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo('stylesheet_url'); ?>" />
+		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+		<link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/img/favicon.ico" type="image/x-icon" />
+		<?php wp_head(); ?>
+		</head>
+		<body <?php body_class(get_bloginfo('language')); ?>>
+		<?php
+	}
+
+	function template_footer() {
+		wp_footer();
+		?>
+		</body>
+		</html>
+		<?php
+	}
+
+}
+
+$GLOBALS['oxpeckers_print'] = new Oxpeckers_Print();
