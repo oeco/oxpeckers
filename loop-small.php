9d2903b09e4b07b3fba98629e48622e8d2bd2382
hvalois <hvalois@gmail.com>
2013-10-04 18:06:24 -0300

    more dev

--- loop-small.php
+++ loop-small.php
@@ -6,7 +6,7 @@
 							<div class="thumb-post">
 							<?php 
 								if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
-								the_post_thumbnail('thumbnail');
+								the_post_thumbnail('small-thumb');
 								} 
 							?>
 							</div>
