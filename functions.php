5ca357be7fac409effa1fb9e3e22a95acb9395cd
hvalois <hvalois@gmail.com>
2013-10-08 15:00:24 -0300

    css addition

--- functions.php
+++ functions.php
@@ -8,15 +8,13 @@ function oxpeckers_scripts() {
 	wp_enqueue_style( 'appearance', get_stylesheet_directory_uri() . '/css/appearance.css' );
 
 	wp_enqueue_script('carousel', get_stylesheet_directory_uri() . '/js/carousel.js', array('jquery'));
-	
-	//wp_enqueue_script( 'script-name', get_template_directory_uri() . '/js/example.js', array(), '1.0.0', true );
 }
 
 add_action('wp_enqueue_scripts', 'oxpeckers_scripts', 100);
 
 function home_page_query($query) {
 	if($query->is_main_query() && (is_front_page() || is_home())) {
-		$query->set('cat', 5);
+		$query->set('category_name', 'oxpeckers-investigations');
 		$query->set('posts_per_page', 4);
 	}
 	return $query;
@@ -33,10 +31,8 @@ add_filter('jeo_marker_base_query', 'map_marker_query');
 
 
 add_theme_support( 'post-thumbnails' );
-add_image_size( 'medium', 300, 140, true );
-add_image_size( 'post-thumb', 300, 140, true );
-add_image_size( 'highlights-thumb', 140, 140, true );
-add_image_size( 'small-thumb', 100, 100, true );
+add_image_size( 'highlights-thumb', 140, 140 );
+add_image_size( 'small-thumb', 100, 100 );
 
 include_once(STYLESHEETPATH . '/inc/print/print.php');
 include_once(STYLESHEETPATH . '/inc/rhino-crisis/rhino-crisis.php');
