5c7a2d25ab7a6280286f0bf1dd09c15efe9156ea
Miguel Peixe <miguel@cardume.art.br>
2013-10-09 14:25:37 -0300

    include related posts to post content

--- functions.php
+++ functions.php
@@ -12,22 +12,22 @@ function oxpeckers_scripts() {
 
 add_action('wp_enqueue_scripts', 'oxpeckers_scripts', 100);
 
-function home_page_query($query) {
+function oxpeckers_home_page_query($query) {
 	if($query->is_main_query() && (is_front_page() || is_home())) {
 		$query->set('category_name', 'oxpeckers-investigations');
 		$query->set('posts_per_page', 4);
 	}
 	return $query;
 }
-add_action('pre_get_posts', 'home_page_query');
+add_action('pre_get_posts', 'oxpeckers_home_page_query');
 
-function map_marker_query($query) {
+function oxpeckers_map_marker_query($query) {
 	if(is_front_page() || is_home())
 		$query = new WP_Query();
 
 	return $query;
 }
-add_filter('jeo_marker_base_query', 'map_marker_query');
+add_filter('jeo_marker_base_query', 'oxpeckers_map_marker_query');
 
 
 add_theme_support( 'post-thumbnails' );
@@ -38,5 +38,42 @@ include_once(STYLESHEETPATH . '/inc/print/print.php');
 include_once(STYLESHEETPATH . '/inc/rhino-crisis/rhino-crisis.php');
 include_once(STYLESHEETPATH . '/inc/geocode-box.php');
 include_once(STYLESHEETPATH . '/inc/submit-story.php');
+
+/*
+ * Include related posts to post content
+ */
+
+function oxpeckers_related_posts_content($content) {
+
+	global $post;
+	$related_posts = get_field('related_posts');
+
+	$output = '';
+	if($related_posts) {
+		$output .= '<h3>Related content</h3>';
+		$output .= '<ul class="related-posts">';
+		foreach($related_posts as $r_post) {
+
+			$post = $r_post;
+			setup_postdata($post);
+
+			$output .= '<li>';
+
+			$output .= '<h4><a href="' . get_permalink() . '" title="' . get_the_title() . '">' . get_the_title() . '</a></h4>';
+			$output .= '<p>' . get_the_excerpt() . '</p>';
+
+			$output .= '</li>';
+
+			wp_reset_postdata();
+
+		}
+	}
+
+	$content = $content . $output;
+
+	return $content;
+
+}
+add_filter('the_content', 'oxpeckers_related_posts_content');
 
 ?>
