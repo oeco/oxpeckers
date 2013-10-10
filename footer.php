0d8f192e3663c6cfbf05f31cb5c726af4ba7de8d
Miguel Peixe <miguel@cardume.art.br>
2013-10-10 01:48:57 -0300

    fix footer

--- footer.php
+++ footer.php
@@ -1,46 +1,42 @@
-<div class="brands clearfix">
-	<div class="partners">
-		<h4><?php _e('Partners', 'oxpeckers'); ?></h4>
-	    <div class="ami">AMI</div>
-	    <div class="chinaafrica">China Africa Reporting Project</div>
-	    <div class="ecolab">Ecolab</div>
-	    <div class="fair">Fair</div>
-	    <div class="hacks">HacksHackers Africa</div>
+	<div class="brands clearfix">
+		<div class="partners">
+			<h4><?php _e('Partners', 'oxpeckers'); ?></h4>
+		    <div class="ami">AMI</div>
+		    <div class="chinaafrica">China Africa Reporting Project</div>
+		    <div class="ecolab">Ecolab</div>
+		    <div class="fair">Fair</div>
+		    <div class="hacks">HacksHackers Africa</div>
+		</div>
+		<div class="footer-brands">
+			<h4><?php _e('Project by', 'oxpeckers'); ?></h4>
+			<div class="ancir">ANCIR</div>
+			<div class="anic">ANIC</div>
+		</div>
 	</div>
-	<div class="footer-brands">
-		<h4><?php _e('Project by', 'oxpeckers'); ?></h4>
-		<div class="ancir">ANCIR</div>
-		<div class="anic">ANIC</div>
-	</div>
-</div>
 
-<footer id="sidebar-footer">
-	<div class="container">
-    
-					<aside id="sidebar-footer">
-						<ul class="widgets">
-							<?php dynamic_sidebar('general'); ?>
-						</ul>
-					</aside>
-</footer>
+	<footer>
+		<aside id="sidebar-footer">
+			<ul class="widgets">
+				<?php dynamic_sidebar('general'); ?>
+			</ul>
+		</aside>
+	</footer>
 
 	<div class="footer-menu">
 
-	<div class="clearfix">
-		<nav id="footer-nav">
-			<?php wp_nav_menu(array('theme_location' => 'footer_menu')); ?>
-		</nav>
-	</div>
-
-
-	<div class="clearfix">
-
-
-		<div class="credits">
-					<p class="credits"><?php printf(__('This website is built on <a href="%s" target="_blank" rel="external">WordPress</a> using the <a href="%s" target="_blank" rel="external">JEO Beta</a> theme', 'jeo'), 'http://wordpress.org', 'http://jeo.cardume.art.br/'); ?></p>
+		<div class="clearfix">
+			<nav id="footer-nav">
+				<?php wp_nav_menu(array('theme_location' => 'footer_menu')); ?>
+			</nav>
 		</div>
-	</div>
-	
+
+
+		<div class="clearfix">
+			<div class="credits">
+				<p class="credits"><?php printf(__('This website is built on <a href="%s" target="_blank" rel="external">WordPress</a> using the <a href="%s" target="_blank" rel="external">JEO Beta</a> theme', 'jeo'), 'http://wordpress.org', 'http://jeo.cardume.art.br/'); ?></p>
+			</div>
+		</div>
+
 	</div>
 
 </div><!-- end oxbody -->
