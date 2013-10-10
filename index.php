c0471d1ca46e09e61c25786d03421b7337c1bd78
Miguel Peixe <miguel@cardume.art.br>
2013-10-10 01:09:09 -0300

    investigations

--- index.php
+++ index.php
@@ -22,6 +22,51 @@ if(is_home() || is_front_page()) {
 			<h2><?php _e('Oxpeckers Investigations', 'jeo'); ?></h2>
 			<?php get_template_part('loop'); ?>
 
+			<?php
+			$investigation_cat = get_category_by_slug('investigations');
+			$investigations = get_categories(array('child_of' => $investigation_cat->term_id, 'hide_empty' => false));
+			if($investigations) :
+				?>
+				<div class="investigations-cats">
+					<ul>
+						<?php
+						foreach($investigations as $investigation) : 
+							$img = get_field('cat_image', 'category_' . $investigation->term_id);
+							?>
+								<li class="<?php echo $investigation->slug; ?> investigation">
+									<?php if($img) : ?>
+										<div class="investigation-thumb-container">
+											<a href="<?php echo get_term_link($investigation); ?>" title="<?php echo $investigation->name; ?>"><img src="<?php echo $img['sizes']['thumbnail']; ?>" class="investigation-thumb" /></a>
+										</div>
+									<?php endif; ?>
+									<h3><a href="<?php echo get_term_link($investigation); ?>" title="<?php echo $investigation->name; ?>"><?php echo $investigation->name; ?></a></h3>
+									<p><?php echo $investigation->description; ?></p>
+								</li>
+							<?php
+						endforeach;
+						?>
+					</ul>
+				</div>
+				<?php
+			endif;
+			?>
+			<style>
+				.investigations-cats ul {
+					display: table;
+					width: 100%;
+				}
+				.investigations-cats ul li {
+					text-align: center;
+					display: table-cell;
+					vertical-align: middle;
+					margin-bottom: 20px;
+				}
+				.investigations-cats ul li img {
+					border-radius: 75px;
+					margin-bottom: 20px;
+				}
+			</style>
+
 			<!--<?php //query_posts(array('category_name' => 'reports', 'posts_per_page' => 3)); ?>
 			<div class="more-stories">
 				<h3><?php //_e('More stories', 'jeo'); ?></h3>
